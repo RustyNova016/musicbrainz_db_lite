79b64ed72c8b2798002d04c645ee31308c53cc7e
RustyNova <rusty.nova.jsb@gmail.com>
2024-12-13 08:22:48 +0000

    fix: `recording_msid` deletetion not cascading

--- musicbrainz_db_lite_schema/src/tables/listenbrainz/listens.rs
+++ musicbrainz_db_lite_schema/src/tables/listenbrainz/listens.rs
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS "listens" (
     `id` INTEGER PRIMARY KEY NOT NULL,
     `listened_at` INTEGER NOT NULL,
     `user` TEXT NOT NULL REFERENCES `users`(`name`) ON DELETE CASCADE,
-    `recording_msid` TEXT NOT NULL REFERENCES `messybrainz_submission`(`msid`),
+    `recording_msid` TEXT NOT NULL REFERENCES `messybrainz_submission`(`msid`) ON DELETE CASCADE,
     `data` TEXT
 
     
