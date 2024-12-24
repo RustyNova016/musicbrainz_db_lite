ce61803d61b32a49caa50f130440262b05298d98
RustyNova <50844553+RustyNova016@users.noreply.github.com>
2024-12-24 08:31:29 +0000

    feat: release labels

---
+++ src/models/musicbrainz/release/relations/label_infos.rs
@@ -0,0 +1,29 @@
+use sqlx::SqliteConnection;
+
+use crate::models::musicbrainz::release::LabelInfo;
+use crate::models::musicbrainz::release::Release;
+
+impl Release {
+    pub async fn get_label_infos_or_fetch(
+        &self,
+        conn: &mut SqliteConnection,
+    ) -> Result<Vec<LabelInfo>, crate::Error> {
+        // First, make sure all the data of the entity is in the database
+        let id = self.get_or_fetch_as_complete(conn).await?.id;
+
+        // Next, get all the children
+        Ok(sqlx::query_as!(
+            LabelInfo,
+            "SELECT
+                    label_infos.*
+                FROM 
+                    releases
+                    INNER JOIN label_infos ON releases.id = label_infos.release
+                WHERE
+                    releases.id = ?",
+            id
+        )
+        .fetch_all(conn)
+        .await?)
+    }
+}
